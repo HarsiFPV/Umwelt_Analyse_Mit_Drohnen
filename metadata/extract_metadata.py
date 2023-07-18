d71883e59c8d13324ab7daf7b4ff718f4b76e37f
Tristan Kraft <harsifpv@gmail.com>
2023-07-18 13:35:46 +0200

    Added working gps extraction to project

--- metadata/extract_metadata.py
+++ metadata/extract_metadata.py
@@ -1,29 +1,17 @@
 import os
+from GPSPhoto import gpsphoto
+import exifread
 
+def extract_image_metadata(image_folder_path, output_file):
 
-def extract_image_metadata(image_folder_path, metadata_file_path):
-    # Create a new file to store the metadata in
-    metadata_file = open(metadata_file_path, "w")
+    os.chdir(image_folder_path)
+    image_list = os.listdir()
+    image_list = [a for a in image_list if a.endswith('.JPG')]
 
-    # Loop through all the files in the image folder
-    for file_name in os.listdir(image_folder_path):
-        # Get the full path of the file
-        file_path = os.path.join(image_folder_path, file_name)
+    with open(output_file, 'w') as f:
+        for image_file in image_list:
+            image_path = os.path.join(image_folder_path, image_file)
+            data = gpsphoto.getGPSData(image_path)
+            f.write(f"File: {image_file} "+"GPS Data: "+str(data)+"\n")
 
-        # Open the image file using the Pillow library
-        img = Image.open(file_path)
-
-        # Extract the metadata from the image
-        metadata = img.getexif()
-
-        print(metadata)
-
-        # Write the metadata to the file as key-value pairs
-        for tag, value in metadata.items():
-            metadata_file.write(f"{file_name} - {tag}: {value}\n")
-
-        metadata_file.write("\r")
-
-    # Close the file when we're done
-    metadata_file.close()
-
+extract_image_metadata(f"E:\Projet6", f"metadata.txt")

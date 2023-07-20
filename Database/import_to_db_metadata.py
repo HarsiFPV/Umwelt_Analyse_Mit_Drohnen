02af11112797d2b895a90eb0567debf557217467
Tristan Kraft <harsifpv@gmail.com>
2023-07-20 13:09:58 +0200

    Modified the import db metadata file to only import new documents

--- Database/import_to_db_metadata.py
+++ Database/import_to_db_metadata.py
@@ -1,24 +1,16 @@
 import pymongo
 
-def import_to_db_metadata(txt_file, url, db, collection):
+def import_to_db_metadata(txt_file, url, db, collection_name):
     # Connect to the MongoDB server
     client = pymongo.MongoClient(url)
 
     # Select the database
     db = client[db]
 
-    # Check if the collection exists, create it if it doesn't
-    if collection not in db.list_collection_names():
-        db.create_collection(collection)
-    collection = db[collection]
-
-    # Clear the collection
-    collection.delete_many({})
-    #print(f"Cleared the collection.")
+    collection = db[collection_name]
 
     with open(txt_file, 'r') as f:
         for line in f:
-
             # Extraction des informations de ligne
             file_info, gps_info = line.split("GPS Data: ")
             _, file_name = file_info.strip().split("File: ")
@@ -30,18 +22,32 @@ def import_to_db_metadata(txt_file, url, db, collection):
             longitude = gps_info["longitude"]
             altitude = gps_info["altitude"]
 
-            # Création du document à insérer dans la collection
-            document = {
-                "data": {
-                    "File": file_name,
-                    "GPS": {
-                        "latitude": latitude,
-                        "longitude": longitude,
-                        "altitude": altitude
+            # Recherche d'un document avec le même nom de fichier dans la collection
+            existing_document = collection.find_one({"data.File": file_name})
+
+            # Si un document avec le même nom de fichier existe, mise à jour des données GPS
+            if existing_document:
+                collection.update_one(
+                    {"data.File": file_name},
+                    {"$set": {
+                        "data.GPS.latitude": latitude,
+                        "data.GPS.longitude": longitude,
+                        "data.GPS.altitude": altitude
+                    }}
+                )
+            else:
+                # Création du document à insérer dans la collection
+                document = {
+                    "data": {
+                        "File": file_name,
+                        "GPS": {
+                            "latitude": latitude,
+                            "longitude": longitude,
+                            "altitude": altitude
+                        }
                     }
                 }
-            }
-            # Insertion du document dans la collection MongoDB
-            collection.insert_one(document)
+                # Insertion du document dans la collection MongoDB
+                collection.insert_one(document)
 
 import_to_db_metadata(r"E:\Projet6\Données\metadata.txt","mongodb://localhost:27017/","P6","file_metadata")
